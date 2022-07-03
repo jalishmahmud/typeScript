3d692b0a5160d08da78c482697271214792cd534
jalishmahmud <jalish93@gmail.com>
2022-07-03 17:54:51 +0600

    array object function example done

--- basic-typescript-syntex/output/script.js
+++ basic-typescript-syntex/output/script.js
@@ -1,3 +1,26 @@
 "use strict";
-const country = "bangladesh is our country";
-console.log(country);
+let playerName = "Sakib Al Hasan";
+console.log(playerName);
+// playerName = 44;
+console.log(playerName);
+let something;
+something = "Jalish";
+console.log(something);
+something = 30;
+console.log(something);
+function plus(a, b) {
+    console.log(a + b);
+}
+console.log(plus(3, 4));
+// array
+const arr = ["jalish", 33, true];
+arr.push({
+    collage: "Raipur"
+});
+// object
+let person = {
+    name: "Sabik",
+    age: 32,
+    isCaptain: true
+};
+person.age = 33;
