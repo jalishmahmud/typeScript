6d1df5bcf975ad19eaf7b6aa5b6b9779f3b7eab3
jalishmahmud <jalish93@gmail.com>
2022-07-08 20:01:19 +0600

    class and access modifire done

--- basic-typescript-syntex/output/classes.js
+++ basic-typescript-syntex/output/classes.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log(7);
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} form ${this.country} is playing`);
+    }
+}
+const mashrafi = new Player('Mashrafi', 40, "Bangladesh");
+const shakib = new Player("Shakib", 30, "Bangladesh");
+const players = [];
+players.push(shakib);
+players.push(mashrafi);
+// class access modifiers
+class Student {
+    /*  name: string;
+        age: number;
+        country: string; */
+    /*  constructor(n: string, a: number, c: string) {
+         this.name = n;
+         this.age = a;
+         this.country = c;
+     } */
+    // shortcut
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    play() {
+        console.log(`${this.name} form ${this.country} is playing`);
+    }
+}
+const kamal = new Student('Mashrafi', 40, "Bangladesh");
+const students = [];
--- basic-typescript-syntex/src/classes.ts
+++ basic-typescript-syntex/src/classes.ts
@@ -1,1 +1,52 @@
-console.log(7)
+class Player {
+    name: string;
+    age: number;
+    country: string;
+
+    constructor(n: string, a: number, c: string) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} form ${this.country} is playing`)
+    }
+}
+
+const mashrafi = new Player('Mashrafi', 40, "Bangladesh");
+const shakib = new Player("Shakib", 30, "Bangladesh")
+
+const players: Player[] = [];
+
+players.push(shakib)
+players.push(mashrafi)
+
+// class access modifiers
+
+class Student {
+   
+/*  name: string;
+    age: number;
+    country: string; */
+    
+   /*  constructor(n: string, a: number, c: string) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    } */
+    // shortcut
+    constructor(
+        public name: string,
+        private age: number,
+        readonly country: string,
+    ) {}
+
+    play() {
+        console.log(`${this.name} form ${this.country} is playing`)
+    }
+}
+
+const kamal = new Student('Mashrafi', 40, "Bangladesh");
+
+
+const students: Student[] = [];
